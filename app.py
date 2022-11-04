5454bbcc563fa71654f0c62ab4066b44e0474f73
Mayank Jha <jhamaynk159@gmail.com>
2022-11-04 23:05:09 +0530

    random port

--- app.py
+++ app.py
@@ -1,61 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request, g,json,session,jsonify
+import random
 
 app = Flask(__name__)
 app.secret_key = "hjtTO6DZulWQyqSYOfuyjXkLYOTVhQBfBkAmUD6UNATf42hX2pSJhlHw1OpWr4KL"
 
 
-@app.route('/devices', methods=['POST'])
+@app.route('/test', methods=['POST'])
 def devices():
     content = request.json
     uid=content['uid']
     #funcion of docker data return
-    print(uid)
-    data= [
-        {
-            "id": 1,
-            "name": "Tubelight"
-
-        },
-
-        {
-            "id": 2,
-            "name": "Fan"
-
-        },
-
-        {
-            "id": 3,
-            "name": "Aquarium"
-
-        },
-
-        ]
+    #print(uid)
+    data= {
+        'uid':uid,
+        'port':random.randrange(2000,6000)
+        }
     
-    return jsonify(data)
-
-@app.route('/devices/<id>', methods = ['POST'])
-def iddata(id):
-    #docker funtion here
-
-    data = {
-        "id":id,
-        "Name": "Device " + id,
-        "Status": "Working",
-        "Value": 3.14
-    }
-
-    return jsonify(data)
-
-@app.route('/devices/<id>/<cmd>', methods = ['POST'])
-def cmddata(id,cmd):
-    #docker funtion here
-    data = {
-        "id":id,
-        "Name": "Device " + id,
-        "Status": cmd,
-        "Value": 3.14
-    }
-
     return jsonify(data)
 
 
